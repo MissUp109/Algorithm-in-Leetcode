6276b4367f1c1620ac0db3f2b12b8a5eaa1644b9
MilkCan <1157974473@qq.com>
2018-12-28 10:54:30 +0800

    upload p34 p35

--- 34_FindFirstAndLastPositionOfElementInSortedArray.cpp
+++ 34_FindFirstAndLastPositionOfElementInSortedArray.cpp
@@ -11,46 +11,88 @@ static const auto io_sync_off = []()
     return nullptr;
 }();
 
+//Approach 1: by Leah
+//Runtime: 4 ms, faster than 100.00% of C++ online submissions
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ret = {-1, -1};
         int l = 0, r = nums.size() - 1;
-        bool flag = false;
-        binarySearch(nums, target, l, r, flag);
-        if(flag){
-        	ret[0] = l; ret[1] = r;
-        	cout << "flag = true" << endl;
-        }
+        binarySearch(nums, target, l, r, ret);
         return ret;
     }
-    void binarySearch(vector<int>& nums, int target, int& l, int& r, bool& flag){
-    	while(l < r){
-    		int m = (l + r) / 2;
-    		cout << "m: " << m << endl;
+    void binarySearch(vector<int>& nums, int target, int l, int r, vector<int>& range){
+        int m;
+    	while(l <= r){
+            //cout << "l: " << l << ", r:" << r << endl;
+    		m = (l + r) / 2;
+            //cout << "m: " << m << endl;
     		if(nums[m] == target){
-    			int cm = m;
-    			flag = true;
-    			binarySearch(nums, target, l, cm - 1, flag);
-    			binarySearch(nums, target, cm + 1, r, flag);
-    			break;
+                if(range[0] == -1 && range[1] == -1){
+                    range[0] = m; range[1] = m;
+                }
+                else{
+                    range[0] = (m < range[0]) ? m : range[0];
+                    range[1] = (m > range[1]) ? m : range[1];
+                }
+
+                binarySearch(nums, target, l, m - 1, range);
+                binarySearch(nums, target, m + 1, r, range);
+
+                break;
     		}
-    		else if(m < target) {
-    			l = m + 1;
-    			//cout << "l: " << l << endl;
-    		}
-    		else{
-    			r = m - 1;
-    			//cout << "r: " << r << endl;
-    		}
+    		else if(nums[m] < target) l = m + 1;
+    		else r = m - 1;	
     	}
     }
 };
 
+
+//Approach 2: by others
+/*
+class Solution {
+    // returns leftmost (or rightmost) index at which `target` should be
+    // inserted in sorted array `nums` via binary search.
+private:
+    int extremeInsertionIndex(vector<int>& nums, int target, bool left) {
+        int lo = 0;
+        int hi = nums.size();
+
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] > target || (left && target == nums[mid])) {
+                hi = mid;
+            }
+            else lo = mid + 1;
+        }
+
+        return lo;
+    }
+
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> targetRange = {-1, -1};
+
+        int leftIdx = extremeInsertionIndex(nums, target, true);
+
+        // assert that `leftIdx` is within the array bounds and that `target`
+        // is actually in `nums`.
+        if (leftIdx == nums.size() || nums[leftIdx] != target) {
+            return targetRange;
+        }
+
+        targetRange[0] = leftIdx;
+        targetRange[1] = extremeInsertionIndex(nums, target, false) - 1;
+
+        return targetRange;
+    }
+};
+*/
+
 int main(){
 	Solution sol;
-	vector<int> nums = {5, 7, 7, 8, 8, 10};
-	int target = 8;
+	vector<int> nums = {1};
+	int target = 0;
 	vector<int> ret = sol.searchRange(nums, target);
 	cout << ret[0] << " " << ret[1] << endl;
 }
@@ -64,5 +106,9 @@ Example 2:
 
 Input: nums = [5,7,7,8,8,10], target = 6
 Output: [-1,-1]
+
+00122, 2
+1,1
+123,1
 */
 
